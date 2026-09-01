c2b7e3c262aa7f5f703094dfbfe1b24618033146
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2026-09-01 14:15:13 +0000

    fé

--- aula5/index.js
+++
@@ -1,6 +0,0 @@
-console.log("Olá");
-<script>
-        //Um comentário
-        console.log("Olá");
-        console.log('Esse trechoi será exibido no console do navegador')
-    </script>
---
+++ aula5/js/index.js
@@ -0,0 +1,2 @@
+console.log("Olá");
+console.log('Esse trechoi será exibido no console do navegador')
