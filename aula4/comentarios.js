a26769735bc8301bb0f85fa110ccf853e1480dea
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2026-09-01 02:14:22 +0000

    obito

---
+++ aula4/comentarios.js
@@ -0,0 +1,13 @@
+//Escrever comentário
+console.log("Olá Mundo");
+console.log("Linha 1");
+
+//Comentário
+
+/*Um bloco de linha
+Linhade comentário
+*/
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+//Vai ser ignorado pelo motor do js
