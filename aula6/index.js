224e05a31357abd6d5fe5c435145977d4e86f2ba
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2026-09-08 14:33:47 +0000

    denji

---
+++ aula6/index.js
@@ -0,0 +1,49 @@
+let nome;// Declarou a variável
+
+nome = 'João' // Inicializando a variável
+console.log(nome);
+nome = 'Luis'
+console.log(nome);
+
+//Utilizamos camelCasel
+let nomeCompletodoCliente = "Luis Felipe"
+
+// Case-sensitive
+let nomeCliente = 'Ana';
+let nomecliente = 'Clara';
+
+console.log(nomeCliente, nomecliente);
+
+//NÃO UTILIZE VAR, UTILIZE LET
+
+/*
+let nome = "Henrique";// Utiliza let para criar variáveis / String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log0('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+|||ERROS|||
+
+//Não podemos criar variáveis com palavras reservadas
+
+let if;
+let console;
+let else;
+let while;
+
+//Não podemos criar variáveis com palavras reservadas
+let n = Luis;
+
+
+//Não pode começar o nome de uma variável com número
+let 1idade;
+
+//Não podem ter espaços ou traços
+let nome-Cliente;
+
+//Não podemos recriar variáveis let
+
+*/
---
+++ aula7/index.js
@@ -0,0 +1,27 @@
+const nome = "João";
+
+
+console.log(nome);
+
+// String = Text | Number = Numero
+const primeroNumero = '5'; // 510 concatenar
+const segundoNumero = 10;
+const resultado = primeroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+// * + - /
+console.log(typeof primeroNumero + segundoNumero);
+// console.log(resultadoTriplicado);
+
+
+
+//Não podemos criar constantes com palavras reservadas
+//Não pode começar o nome de uma constante com número
+//Não podem ter espaços ou traços
+//Não podemos recriar variáveis let
+//Constantes precisam ter nomes significativos
+//Não podemos modificar o valor de uma constante
+// Case-sensitive
+//Utilizamos camelCasel
+//NÃO UTILIZE VAR, UTILIZE LET
